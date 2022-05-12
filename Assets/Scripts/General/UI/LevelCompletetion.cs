d54871871901edc71612d1b3b86d394d5d5c71ff
crismellaxz91 <cristobal.mella@uniacc.edu>
2022-05-12 15:42:40 -0400

    Adding win menu

--- Assets/Scripts/General/UI/LevelCompletetion.cs
+++ Assets/Scripts/General/UI/LevelCompletetion.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class LevelCompletetion : MonoBehaviour
 {
-    public TMP_Text tMP_Text;
-    public TMP_Text youWin;
+    public TMP_Text textoNumeroDeEnemigos;
+    public GameObject endMenu;
     public int enemiesLeft;
-    public GameObject tutorial;
+    //public GameObject winMenu;
     public void Update()
     {
         GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
         enemiesLeft = enemigos.Length;
-        tMP_Text.text = "Quedan" + " " + enemiesLeft.ToString() + "espíritus";
-        if (enemiesLeft == 0)
+        textoNumeroDeEnemigos.text = "Quedan" + " " + enemiesLeft.ToString() + "espíritus";
+        if (enemiesLeft <= 0)
         {
-            tMP_Text.enabled = false;
-            youWin.enabled = true;
+            textoNumeroDeEnemigos.enabled = false;
+            endMenu.SetActive(true);
         }
     }
 }
