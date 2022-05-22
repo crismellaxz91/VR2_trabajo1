4e094da19de72017182ebf129ca59155685266e7
CMGxz91 <cristobal.mella@uniacc.edu>
2022-05-22 14:12:06 -0400

    evento para fuego

---
+++ Assets/Scripts/General/FireEvents.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+public class FireEvents : MonoBehaviour
+{
+    public UnityEvent fireEvent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var water = other.GetComponent<FreezingStatus>();
+        if (water != null)
+        {
+            fireEvent.Invoke();
+        }
+    }
+    #endregion
+}
