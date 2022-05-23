6a9dd91f33027d544b8d0ddf672a6315f1af9742
CMGxz91 <cristobal.mella@uniacc.edu>
2022-05-23 13:02:19 -0400

    added portal visuals

---
+++ Assets/Portal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Transform backPortal;
+    public Transform entrancePortal;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
