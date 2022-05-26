6423a317f0a31b11b6975dafe68758bf0fe48edf
CMGxz91 <cristobal.mella@uniacc.edu>
2022-05-25 22:20:36 -0400

    added working portals

--- Assets/Scripts/General/Portal.cs
+++ Assets/Scripts/General/Portal.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public Transform teleportPosition;
+    public Animator teleportLoad;
     private void OnTriggerEnter(Collider other)
     {
       if(other.CompareTag("Player"))
         {
-
+            teleportLoad.enabled = true;
+        }
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
             other.transform.position = teleportPosition.position;
         }
     }
