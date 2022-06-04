b0d3f8d4c66850d4066d5e474aceeab8be911c9c
CMGxz91 <cristobal.mella@uniacc.edu>
2022-06-04 18:20:23 -0400

    working HP item added

--- Assets/Scripts/General/HelpPlayer/RestoreHP.cs
+++ Assets/Scripts/General/HelpPlayer/RestoreHP.cs
@@ -7,22 +7,25 @@ public class RestoreHP : MonoBehaviour
     [SerializeField]
     private int hpToRestore;
     [SerializeField]
-    private int hpMax;
-    [SerializeField]
     private Health playerHealth;
 
     public void Awake()
     {
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
     }
-    public void OnTriggerEnter(Collider other)
+    public void OnCollisionEnter(Collision collision)
     {
-        if(other.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player"))
         {
-            if(playerHealth.health < 20 && playerHealth.health > 0)
+            if (playerHealth.health < 20 && playerHealth.health > 0)
             {
                 playerHealth.health += hpToRestore;
+                if (playerHealth.health > 20)
+                {
+                    playerHealth.health = 20;
+                }
             }
+            Destroy(gameObject);
         }
     }
 }
