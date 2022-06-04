fdfe265be70a6a63bee9cd7a90a816eacc286f2c
CMGxz91 <cristobal.mella@uniacc.edu>
2022-06-04 18:43:12 -0400

    working HP drop added

--- Assets/Scripts/General/HelpPlayer/DropHP.cs
+++ Assets/Scripts/General/HelpPlayer/DropHP.cs
@@ -6,13 +6,18 @@ public class DropHP : MonoBehaviour
 {
     public GameObject hpDrop;
     public float instantiateLaunchSpeed;
+    public int itemsToDrop;
     public void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("PushBullet"))
         {
-            Instantiate(hpDrop, transform.position, Quaternion.Euler(0, Random.Range(0, 4) * 90, 0));
-            Rigidbody rb = hpDrop.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.up * instantiateLaunchSpeed;
+            if(itemsToDrop <3)
+            {
+                Instantiate(hpDrop, transform.position, Quaternion.Euler(0, Random.Range(0, 4) * 90, 0));
+                itemsToDrop++;
+                Rigidbody rb = hpDrop.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.up * instantiateLaunchSpeed;
+            }
         }
     }
 }
