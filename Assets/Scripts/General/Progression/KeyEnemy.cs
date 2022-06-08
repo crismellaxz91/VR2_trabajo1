f2a76c99b784f2753d8ec7e547516e3ea0242ead
CMGxz91 <cristobal.mella@uniacc.edu>
2022-06-07 22:24:47 -0400

    making level progress part 1

--- Assets/Scripts/General/Progression/KeyEnemy.cs
+++ Assets/Scripts/General/Progression/KeyEnemy.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class KeyEnemy : MonoBehaviour
 {
-    
+    public Health hpKeyEnemy;
+    public ProgressLVL progress;
+    public void Update()
+    {
+       hpKeyEnemy = gameObject.GetComponent<Health>();
+        if (hpKeyEnemy.health <= 0)
+        {
+            OnDeath();
+        }
+    }
+    public void OnDeath()
+    {
+        
+    }
 }
