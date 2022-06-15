40000d6ce2e0855f3c0ef13447d920f567ac3551
CMGxz91 <cristobal.mella@uniacc.edu>
2022-06-15 15:57:55 -0400

    adding Boss 2

--- Assets/Scripts/Enemies/BossScript/Boss.cs
+++ Assets/Scripts/Enemies/BossScript/Boss.cs
@@ -12,33 +12,68 @@ public class Boss : MonoBehaviour
     [SerializeField]
     bool enableAct;
     int atkStep;
+    public float speedBoss;
+    public float sightRange;
+    public bool playerInSightRange;
+    public LayerMask whatIsPlayer;
     void Start()
     {
         anim = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         enableAct = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        agent = gameObject.GetComponent<NavMeshAgent>();
     }
     public void ChasePlayer()
     {
-        if((target.position - transform.position).magnitude >= 10)
-        {
-            agent.SetDestination(target.position);
-            anim.SetBool("Walk", true);
-        }
-        if((target.position - transform.position).magnitude < 10)
-        {
-            anim.SetBool("Walk", false);
-        }
+        agent.SetDestination(target.position);
+        anim.SetBool("Walk", true);
     }
     public void RotateBoss()
     {
       Vector3 dir = target.transform.position - transform.position;
-      transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime);
+      transform.localRotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime);
+    }
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        if (enableAct)
+        {
+            RotateBoss();
+            ChasePlayer();
+        }
+    }
+    void BossAttack()
+    {
+        if((target.position - transform.position).magnitude < 10)
+        {
+            switch(atkStep)
+            {
+                case 0:
+                    atkStep += 1;
+                    anim.Play("");
+                    break;
+                case 1:
+                    atkStep += 1;
+                    anim.Play("");
+                    break;
+                case 2:
+                    atkStep = 0;
+                    anim.Play("");
+                    break;
+            }
+        }
+    }
+    void FreezeBoss()
+    {
+        enableAct = false;
+    }
+    void UnFreezeBoss()
+    {
+        enableAct = true;
+    }
+    private void OnDrawGizmosSelected() // visualizacion rangos de vision y ataque
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
